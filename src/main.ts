1435e02799bb7b3d55aa99f6fc76c66301ed969a
hulk510 <dorian.51069@gmail.com>
2024-01-17 18:53:17 +0900

    feat: debugしてないがブランチのキャッシュを削除するactionの作成

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 
 /**
  * The main function for the action.
@@ -6,6 +7,62 @@ import * as core from '@actions/core'
  */
 export async function run(): Promise<void> {
   try {
+    const token = core.getInput('repo-token')
+    const octokit = github.getOctokit(token)
+    // get context
+    // MEMO: git contextからheadRefは取得できなそう
+    const { repo, ref } = github.context
+
+    const headRef = process.env.GITHUB_HEAD_REF
+    core.debug(`headRef: ${headRef}`)
+
+    if (headRef) {
+      // Get the list of cache IDs
+      // https://github.com/octokit/plugin-paginate-rest.js#octokitpaginate
+      const cacheIdList = await octokit.paginate(
+        octokit.rest.actions.getActionsCacheList,
+        {
+          ...repo,
+          ref
+        },
+        response => response.data.flatMap(cache => cache.id ?? [])
+      )
+      if (cacheIdList.length === 0) {
+        core.info('No cache found.')
+        return
+      }
+
+      // Delete the caches
+      cacheIdList.forEach(async id => {
+        await octokit.rest.actions.deleteActionsCacheById({
+          ...repo,
+          cache_id: id
+        })
+      })
+    }
+
+    // Get the list of cache IDs
+    // https://github.com/octokit/plugin-paginate-rest.js#octokitpaginate
+    const cacheIdList = await octokit.paginate(
+      octokit.rest.actions.getActionsCacheList,
+      {
+        ...repo,
+        ref
+      },
+      response => response.data.flatMap(cache => cache.id ?? [])
+    )
+    if (cacheIdList.length === 0) {
+      core.info('No cache found.')
+      return
+    }
+
+    // Delete the caches
+    cacheIdList.forEach(async id => {
+      await octokit.rest.actions.deleteActionsCacheById({
+        ...repo,
+        cache_id: id
+      })
+    })
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
